2a4b9eff0946d73378e77ab93564049ba1f254e8
Samuel Uyigue <Samuel.uyigue@gmail.com>
2022-02-01 07:43:24 -0600

    Initial Commit

---
+++ TodoListDemo/TodoListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListDemoApp.swift
+//  TodoListDemo
+//
+//  Created by Osaretin Uyigue on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
